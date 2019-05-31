31f5b2eff7391305857af4077d4f6cbe5c7a3bfb
anying798 <798617778@qq.com>
2019-05-31 10:58:37 +0800

    first commit

---
+++ src/main/java/com/unicom/myproject/controller/NewsController.java
@@ -0,0 +1,29 @@
+package com.unicom.myproject.controller;
+
+import com.unicom.myproject.entity.News;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/news")
+public class NewsController {
+
+    @RequestMapping(value = "/getAll")
+    public List<News> getAll(){
+        News news1 = new News(1,"aaa","AAAA");
+        News news2 = new News(2,"bbb","BBBB");
+        News news3 = new News(3,"ccc","CCCC");
+        News news4 = new News(4,"ddd","DDDD");
+
+        List<News> list = new ArrayList<>();
+        list.add(news1);
+        list.add(news2);
+        list.add(news3);
+        list.add(news4);
+
+        return list ;
+    }
+}
